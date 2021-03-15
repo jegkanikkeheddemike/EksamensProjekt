ac7b2ef11d90c49a69b2402c426f7a1d5246012c
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2021-03-15 12:37:23 +0100

    main class init

---
+++ Main/src/Main.java
@@ -0,0 +1,28 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static Main main;
+
+    public Main() {
+        main = this;
+    }
+
+    @Override
+    public void settings() {
+        size(1920, 1080);
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void draw() {
+        clear();
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(new Main().getClass());
+    }
+}
