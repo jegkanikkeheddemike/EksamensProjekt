99e5107468aa15b36242b35a83fefc9f7497454c
toreseb <toreseb@gmail.com>
2021-03-15 14:12:04 +0100

    Update GameMath.java

--- Main/src/Framework/GameMath.java
+++ Main/src/Framework/GameMath.java
@@ -8,11 +8,11 @@ public class GameMath {
     }
 
     public static float objectDistance(GameObject o1, GameObject o2) {
-        float x1 = o1.x + o1.width / 2;
-        float y1 = o1.y + o1.height / 2;
+        float x1 = o1.x + o1.w / 2;
+        float y1 = o1.y + o1.h / 2;
 
-        float x2 = o2.x + o2.width / 2;
-        float y2 = o2.y + o2.height / 2;
+        float x2 = o2.x + o2.w / 2;
+        float y2 = o2.y + o2.h / 2;
 
         float dx = x2 - x1;
         float dy = y2 - y1;
