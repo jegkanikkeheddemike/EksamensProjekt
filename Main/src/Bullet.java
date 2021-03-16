366c23b703b29e1cf80343765dc6539e761f4101
toreseb <toreseb@gmail.com>
2021-03-16 15:48:20 +0100

    #29

--- Main/src/Bullet.java
+++ Main/src/Bullet.java
@@ -1,26 +1,55 @@
+
+
 public class Bullet extends Movables{
     float dir;
+    float xDir;
+    float yDir;
+    float damage = 10;
+    float range = 400;
+    float xEnd;
+    float yEnd;
     
     Bullet(float rotation, float x, float y){
         super();
         dir = rotation;
         this.x = Main.player.middleX()-(float)Math.sin(dir-Math.PI/2)*Main.player.w;
-        Main.println((float)Math.sin(dir-Math.PI/2),100,100);
         this.y = Main.player.middleY()+(float)Math.cos(dir-Math.PI/2)*Main.player.h;
-        xSpeed = -(float)Math.sin(dir-Math.PI/2)*10;
-        ySpeed = (float)Math.cos(dir-Math.PI/2)*10;
+        xDir = -(float)Math.sin(dir-Math.PI/2);
+        yDir = (float)Math.cos(dir-Math.PI/2);
         classID = "Bullet";
+        checkCollide();
     }
 
     @Override
     public void draw() {
-        Main.main.circle(x,y,2);
+        Main.main.line(x,y,xEnd,yEnd);
     }
 
     @Override
     public void step() {
-        x += xSpeed;
-        y += ySpeed;
+
+        
     }
+
+    void checkCollide(){
+        LineCollisionData data = new LineCollisionData(false, 0, 0);
+        for (int i = 0; i < Main.nearObjects.size();i++){
+            GameObject object = Main.nearObjects.get(i);
+            
+            if(object.classID == "Wall"||object.classID=="Zombie"){
+                data = GameMath.lineRect(x,y,x+xDir*range,y+yDir*range,object.middleX(),object.middleY(),object.w,object.h);
+                if(GameMath.objectDistance(Main.player, object)<GameMath.pointDistance(Main.player.middleX(), Main.player.middleY(), data.xCol, data.yCol))
+                data = GameMath.lineRect(x,y,x+xDir*range,y+yDir*range,object.middleX(),object.middleY(),object.w,object.h);
+                System.out.println(GameMath.objectDistance(Main.player, object) + " < " + GameMath.pointDistance(Main.player.middleX(), Main.player.middleY(), data.xCol, data.yCol));
+                
+            }
+            
+        }
+        if (data.hasCollided){
+            xEnd = data.xCol;
+            yEnd = data.yCol;
+        }
+    }
+
     
 }
--- Main/src/GameMath.java
+++ Main/src/GameMath.java
@@ -30,6 +30,70 @@ public class GameMath {
 
         float dx = x2 - x1;
         float dy = y2 - y1;
-        return (float) Math.tanh(dy / dx);
+        return (float) Math.atan2(dy,dx);
     }
+
+    // LINE/RECTANGLE
+public static LineCollisionData lineRect(float x1, float y1, float x2, float y2, float rx, float ry, float rw, float rh) {
+
+    // check if the line has hit any of the rectangle's sides
+    // uses the Line/Line function below
+    LineCollisionData left =   lineLine(x1, y1, x2, y2, rx-rw/2, ry-rh/2, rx-rw/2, ry+rh/2);
+    LineCollisionData right =  lineLine(x1, y1, x2, y2, rx+rw/2, ry-rh/2, rx+rw/2, ry+rh/2);
+    LineCollisionData top =    lineLine(x1, y1, x2, y2, rx-rw/2, ry-rh/2, rx+rw/2, ry-rh/2);
+    LineCollisionData bottom = lineLine(x1, y1, x2, y2, rx-rw/2, ry+rh/2, rx+rw/2, ry+rh/2);
+  
+    // if ANY of the above are true, the line
+    // has hit the rectangle
+    LineCollisionData closestCol = new LineCollisionData(false, 0, 0);
+    if (left.hasCollided || right.hasCollided || top.hasCollided || bottom.hasCollided) {
+        if (left.hasCollided)
+            closestCol=left;
+        if (right.hasCollided && (closestCol == null||Main.dist(x1, y1, right.xCol, right.yCol)<Main.dist(x1, y1, closestCol.xCol, closestCol.yCol)))
+            closestCol=right;
+        if (top.hasCollided && (closestCol == null||Main.dist(x1, y1, top.xCol, top.yCol)<Main.dist(x1, y1, closestCol.xCol, closestCol.yCol)))
+            closestCol=top;
+        if (bottom.hasCollided && (closestCol == null||Main.dist(x1, y1, bottom.xCol, bottom.yCol)<Main.dist(x1, y1, closestCol.xCol, closestCol.yCol)))
+            closestCol=bottom;
+    }
+    return closestCol;
+  }
+  
+  
+  // LINE/LINE
+  public static LineCollisionData lineLine(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+  
+    // calculate the direction of the lines
+    float uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+    float uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+  
+    // if uA and uB are between 0-1, lines are colliding
+    if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
+  
+      // optionally, draw a circle where the lines meet
+      
+       float intersectionX = x1 + (uA * (x2-x1));
+       float intersectionY = y1 + (uA * (y2-y1));
+    
+       LineCollisionData data = new LineCollisionData(true,intersectionX,intersectionY);
+
+       
+      return data;
+    }
+    return new LineCollisionData(false, 0, 0);
+  }
+}
+
+class LineCollisionData{
+    public boolean hasCollided;
+    public float xCol;
+    public float yCol;
+
+    LineCollisionData(boolean hasCollided, float xCol, float yCol){
+        this.hasCollided = hasCollided;
+        this.xCol = xCol;
+        this.yCol = yCol;
+    }
+
+
 }
