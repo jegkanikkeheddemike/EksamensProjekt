238e6698c0679649b5816e49b7f74c168a89d895
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2021-05-07 16:35:23 +0200

    Compile test

--- Main/ExecutableSource.cpp
+++ Main/ExecutableSource.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+
 int main() {
-	system("Pause");
 	system("java -jar --enable-preview \"Main.jar\"");
-	std::cout<<"Finished game?\n";
+	std::cout<<"Bemærk, at for at køre dette, kræver det den nyeste version af Java runtime!\n";
 	system("Pause");
 }
