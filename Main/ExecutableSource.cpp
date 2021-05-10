2b38f46a73542848c01274c58adbc0829ccaf478
jegkanikkeheddemike <thor.skipper.hej@gmail.com>
2021-05-10 13:08:29 +0200

    cpp ændring som gør intet..

--- Main/ExecutableSource.cpp
+++ Main/ExecutableSource.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-
-extern const char jarFile[];
-
 int main() {
-
-
 	system("java -jar --enable-preview \"Main.jar\"");
 	std::cout<<"\n\nHusk, at for at spille dette, skal du have den nyeste version af Java runtime OG den nyeste JDK (Java development kit)!\n";
 	system("Pause");
